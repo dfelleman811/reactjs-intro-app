bf6d5a44c71e5206ff4363235c3ee88b61aad8b4
Daniel Felleman <daniel.felleman@revature.com>
2023-03-15 15:18:39 -0400

    added styling to ExpenseItem component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,8 +1,20 @@
 // filenaming convention is to use Pascale Case
 
+import './ExpenseItem.css'; // we can style with standard css styling - just need to be aware of attribute naming that might be different in JSX
+
 // components are just functions that return html code
 function ExpenseItem() {
-    return <h2>Expense Item</h2>
+  // you MUST only have one root or parent element per return statement/jsx code snippet
+  return (
+    // use className attribute to give classes to these elements
+    <div className="expense-item">
+      <div>March 15, 2023</div>
+      <div className="expense-item__description">
+        <h2>Car insurance</h2>
+        <div className="expense-item__price">$90.00</div>
+      </div>
+    </div>
+  );
 }
 // need to export it in order to use it
-export default ExpenseItem
+export default ExpenseItem;
