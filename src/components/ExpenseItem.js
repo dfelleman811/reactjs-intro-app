c45ed2c3fd0edad90d04d0ebe206df262f87c418
Daniel Felleman <daniel.felleman@revature.com>
2023-03-16 14:31:34 -0400

    refactored ExpenseItem date display

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -7,11 +7,20 @@ import './ExpenseItem.css'; // we can style with standard css styling - just nee
 // keys are the attribute names defined in App.js
 function ExpenseItem(props) {
    
+  // better to put excessive logic here rather than in the JSX return - i.e. for the date
+  const month = props.date.toLocaleString('en-US', {month: 'long'})
+  const day = props.date.toLocaleString('en-US', {day: '2-digit'})
+  const year = props.date.getFullYear()
+
   // you MUST only have one root or parent element per return statement/jsx code snippet
   return (
     // use className attribute to give classes to these elements
     <div className="expense-item">
-      <div>{ props.date.toString() }</div>
+      <div>
+        <div>{ month }</div>
+        <div>{ year }</div>
+        <div>{ day }</div>
+      </div>
       <div className="expense-item__description">
         <h2>{ props.title }</h2>
         <div className="expense-item__price">${ props.amount }</div>
