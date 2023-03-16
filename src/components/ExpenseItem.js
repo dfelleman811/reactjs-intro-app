5858af78929fae74ec37f58107d2adf8cbd7db2d
Daniel Felleman <daniel.felleman@revature.com>
2023-03-16 17:09:35 -0400

    added useState to ExpenseItem.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,5 @@
 // filenaming convention is to use Pascale Case
-
+import { useState } from "react";
 import "./ExpenseItem.css"; // we can style with standard css styling - just need to be aware of attribute naming that might be different in JSX
 import ExpenseDate from "./ExpenseDate";
 import Card from "./Card";
@@ -7,17 +7,19 @@ import Card from "./Card";
 // React will ensure we get one parameter - that is an object that holds all the properties. It's called 'props' - with key:value pairs
 // keys are the attribute names defined in App.js
 function ExpenseItem(props) {
-  
+  const [title, setTitle] = useState(props.title);
+
   const clickHandler = () => {
-    console.log("Clicked!")
-  }
+    setTitle("Updated!");
+    //console.log("Clicked!")
+  };
   // you MUST only have one root or parent element per return statement/jsx code snippet
   return (
     // use className attribute to give classes to these elements
     <Card className="expense-item">
-      <ExpenseDate date={props.date}/>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Click here</button>
